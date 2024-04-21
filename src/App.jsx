c938d6eb9523ed1793fe0bf7b8f91f95829bd93c
firecoderr <akhmedovulugbek296@gmail.com>
2024-04-21 21:43:51 +0500

    image change

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
 
         <div className="hero-image-cover">
           <img
-            src="src/assets/avatar.JPG"
+            src="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"
             alt="profile-image"
             className="profile-image"
           />
