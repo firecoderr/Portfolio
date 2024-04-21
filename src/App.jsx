c1860e56ae60e78ea3473df7e3d3d2fe31209776
firecoderr <akhmedovulugbek296@gmail.com>
2024-04-21 19:40:42 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Header from "./components/Header/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header></Header>
+
+      <div className="hero-section">
+        {/* Social Medias */}
+        <div className="social-medias">
+          <a href="#" className="media-links">
+            <p>Instagram</p>
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a href="#" className="media-links">
+            <p>Github</p>
+            <i className="fa-brands fa-github"></i>
+          </a>
+        </div>
+
+        <div className="hero-text">
+          <h1>Ulugbek Akhmedov</h1>
+          <div className="frontend-text">
+            <div className="line"></div>
+            <p>Front-End</p>
+          </div>
+          <p>I'm a creative Designer and I'm very passionate to my work...</p>
+          <button className="hero-button">
+            Say Hello <i class="fa-solid fa-paper-plane"></i>
+          </button>
+        </div>
+
+        <div className="hero-image-cover">
+          <img
+            src="./src/assets/avatar.jpg"
+            alt="profile-image"
+            className="profile-image"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState } from "react";
+import "./Header.css";
+
+export default function Header() {
+  const [toggle, setToggle] = useState(false);
+
+  console.log(toggle);
+
+  return (
+    <>
+      <header>
+        <div className="header-container">
+          <a href="#" className="header-logo">
+            Ulugbek
+          </a>
+
+          <nav className={toggle ? "open" : ""}>
+            <ul>
+              <li>
+                <a href="#" className="nav-link">
+                  home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="nav-link">
+                  about
+                </a>
+              </li>
+              <li>
+                <a href="#" className="nav-link">
+                  projects
+                </a>
+              </li>
+              <li>
+                <a href="#" className="nav-link">
+                  skills
+                </a>
+              </li>
+              <li>
+                <a href="#" className="nav-link">
+                  contacts
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <i
+            className={toggle ? "fa-solid fa-xmark" : "fa-solid fa-bars"}
+            onClick={() => setToggle(!toggle)}
+          ></i>
+        </div>
+      </header>
+    </>
+  );
+}
