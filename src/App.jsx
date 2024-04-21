280a8d3ae7ff4d92f028c3d101a987a5819b42c3
firecoderr <akhmedovulugbek296@gmail.com>
2024-04-21 23:30:52 +0500

    big update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ export default function App() {
     <>
       <Header></Header>
 
-      <div className="hero-section">
+      {/* Hero Section */}
+      <section className="hero-section">
         {/* Social Medias */}
         <div className="social-medias">
           <a href="#" className="media-links">
@@ -33,12 +34,70 @@ export default function App() {
 
         <div className="hero-image-cover">
           <img
-            src="src/assets/avatar.JPG"
+            src="src/assets/Avatar.jpg"
             alt="profile-image"
             className="profile-image"
           />
         </div>
-      </div>
+      </section>
+
+      {/* About Section */}
+      <section className="about-section">
+        <div className="about-top">
+          <h2>About</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+            praesentium sequi obcaecati corporis amet quas ipsum incidunt nemo
+            eveniet cumque id numquam tempore reiciendis, quod ullam eaque fugit
+            nulla possimus!
+          </p>
+        </div>
+
+        <div className="skills">
+          <h3>Skills</h3>
+          <div className="skill-item-container">
+            <div className="skill-item">
+              <i class="fa-brands fa-html5"></i>
+              <h2>HTML5</h2>
+            </div>
+
+            <div className="skill-item">
+              <i class="fa-brands fa-css3-alt"></i>
+              <h2>CSS3</h2>
+            </div>
+
+            <div className="skill-item">
+              <i class="fa-brands fa-js"></i>
+              <h2>JavaScript</h2>
+            </div>
+
+            <div className="skill-item">
+              <i class="fa-brands fa-react"></i>
+              <h2>ReactJS</h2>
+            </div>
+
+            <div className="skill-item">
+              <i class="fa-brands fa-bootstrap"></i>
+              <h2>Bootstrap 5</h2>
+            </div>
+
+            <div className="skill-item">
+              <img
+                src="https://www.svgrepo.com/show/333609/tailwind-css.svg"
+                alt="icon"
+              />
+              <h2>Tailwindcss</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Project Section */}
+      <section className="project-section">
+        <div className="project-top">
+          <h2>Projects</h2>
+        </div>
+      </section>
     </>
   );
 }
